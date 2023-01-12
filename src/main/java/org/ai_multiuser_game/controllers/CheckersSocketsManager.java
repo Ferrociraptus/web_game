fa44492a1a48fcfce6031518cb836a443bb01394
Arseniy Alexeev <ars@alexeev.info>
2023-01-12 23:42:38 +0300

    add: informUserThatOpponentConnectedToGame() method added to CheckersSocketsManager class
    - CheckersSocketsManager server endpoint changed to '/game/socket/{userID}'
    - IDPair were replaced on only userId keys
    - gameID field added to json socket messages

--- src/main/java/org/ai_multiuser_game/controllers/CheckersSocketsManager.java
+++ src/main/java/org/ai_multiuser_game/controllers/CheckersSocketsManager.java
@@ -2,9 +2,9 @@ package org.ai_multiuser_game.controllers;
 
 
 import org.ai_multiuser_game.data.GameDTO;
+import org.ai_multiuser_game.entities.CheckerColor;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
@@ -18,58 +18,35 @@ import javax.websocket.server.ServerEndpoint;
 import javax.websocket.Session;
 
 
-@ServerEndpoint("/game/checker/{gameID}/user/{userID}")
+@ServerEndpoint("/game/socket/{userID}")
 @ApplicationScoped
 public class CheckersSocketsManager {
-    static private class IDPair{
-        public Long gameId;
-        public Long userId;
 
-        public IDPair(Long gameId, Long userId){
-            this.gameId = gameId;
-            this.userId = userId;
-        }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof IDPair idPair)) return false;
-
-            if (!Objects.equals(gameId, idPair.gameId)) return false;
-            return Objects.equals(userId, idPair.userId);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = (gameId != null ? gameId.hashCode() : 0) * 17;
-            result = (result + (userId != null ? userId.hashCode() : 0)) * 31;
-            return result;
-        }
-    }
     private static final Logger logger = Logger.getLogger(CheckersSocketsManager.class.getName());
-    Map<IDPair, Session> sessions = new ConcurrentHashMap<>();
+    Map<Long, Session> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("gameID") Long gameId, @PathParam("userID") Long userId) {
-        sessions.put(new IDPair(gameId, userId), session);
+    public void onOpen(Session session, @PathParam("userID") Long userId) {
+        sessions.put(userId, session);
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("gameID") Long gameId, @PathParam("userID") Long userId) {
-        sessions.remove(new IDPair(gameId, userId));
+    public void onClose(Session session, @PathParam("userID") Long userId) {
+        sessions.remove(userId);
         logger.info("Socket for user %dl closed".formatted(userId));
     }
 
     @OnError
-    public void onError(Session session, @PathParam("gameID") Long gameId, @PathParam("userID") Long userId, Throwable throwable) {
-        sessions.remove(new IDPair(gameId, userId));
+    public void onError(Session session, @PathParam("userID") Long userId, Throwable throwable) {
+        sessions.remove(userId);
         logger.warning("Sockets trouble: " + throwable.getMessage());
     }
-
-    @OnMessage
-    public void onMessage(String message, @PathParam("gameID") Long gameId, @PathParam("userID") Long userId) {
-
-    }
+//
+//    @OnMessage
+//    public void onMessage(String message, @PathParam("userID") Long userId) {
+//
+//    }
 
     private void broadcast(String message) {
         sessions.values().forEach(s -> {
@@ -82,19 +59,35 @@ public class CheckersSocketsManager {
     }
 
     public void finishGameSockets(GameDTO gameData){
-        sessions.remove(new IDPair(gameData.id, gameData.firstUserId));
-        sessions.remove(new IDPair(gameData.id, gameData.secondUserId));
+        sessions.remove(gameData.firstUserId);
+        sessions.remove(gameData.secondUserId);
     }
 
     public void informUserAboutTurn(GameDTO gameData, Long userId) {
-        sessions.get(new IDPair(gameData.id, userId)).getAsyncRemote()
+        sessions.get(userId).getAsyncRemote()
                 .sendObject("{turnAvailable: true}");
     }
 
     public void informUserAboutTurn(Long gameId, Long userId){
-        Session session = sessions.get(new IDPair(gameId, userId));
+        Session session = sessions.get(userId);
         if (session != null)
-            session.getAsyncRemote().sendObject("{turnAvailable: true}");
+            session.getAsyncRemote().sendObject("{\"turnAvailable\": true,\"timestamp\":%s, gameID:%d}"
+                    .formatted(Long.toString(System.currentTimeMillis()), gameId));
+    }
+
+    public void informUserAboutGameEnd(Long gameId, Long userId, CheckerColor winSide){
+        Session session = sessions.get(userId);
+        if (session != null)
+            session.getAsyncRemote().sendObject("{\"gameFinished\": true," +
+                                                    "\"winSide\": \"%s\", \"id\":%s, gameID:%d}".formatted(winSide.name(),
+                                                            Long.toString(System.currentTimeMillis()), gameId));
+    }
+
+    public void informUserThatOpponentConnectedToGame(Long gameId, Long userId){
+        Session session = sessions.get(userId);
+        if (session != null)
+            session.getAsyncRemote().sendObject("{\"isOpponentConnected\": true, \"id\":%s, gameID:%d}".formatted(
+                            Long.toString(System.currentTimeMillis()), gameId));
     }
 }
 
