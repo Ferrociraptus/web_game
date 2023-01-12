ea53e81ec069be49ca68aaaa1bd3d9f29ea65389
Arseniy Alexeev <ars@alexeev.info>
2023-01-12 23:28:28 +0300

    add: CheckerColor translation from RussianCheckers game CheckerColor obj added

--- src/main/java/org/ai_multiuser_game/entities/CheckerColor.java
+++ src/main/java/org/ai_multiuser_game/entities/CheckerColor.java
@@ -1,6 +1,13 @@
 package org.ai_multiuser_game.entities;
 
+
+import org.gamecore.RussianCheckers.Checker;
+
 public enum CheckerColor {
     BLACK,
-    WHITE
+    WHITE;
+
+    static public CheckerColor fromGameColor(Checker.CheckerColor color){
+        return (color == Checker.CheckerColor.Black) ? BLACK : WHITE;
+    }
 }
